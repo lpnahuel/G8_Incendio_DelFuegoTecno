84728324b6caf4251109a1d0fb8b0e4a3c2b336d
lpnahuel <nahuel.larrea@hotmail.com>
2022-05-10 13:29:07 -0300

    product detail dinamico

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,6 +1,6 @@
 const almacenamiento=[
   {
-    "Id": 21,
+    "id": 21,
     "name": "Disco Externo 2 Tb Seagate Game Drive Para Ps4 Usb 3.0",
     "image": "Disco-Externo-2-tb-Seagate-Game-Drive-para-PS4-Usb-30",
     "image_2": "",
@@ -12,7 +12,7 @@ const almacenamiento=[
     "caracteristics": "Compatibilidad:Pc\\nCompatibilidad:Windows\\nCompatibilidad:Playstation 4\\nConectividad:USB 3.0\\nFactor de Forma:2.5 “"
   },
   {
-    "Id": 22,
+    "id": 22,
     "name": "Disco Ssd Adata 1Tb Spectrix Xpg S40G Gen 3X4 M.2 2280",
     "image": "DISCO-SSD-ADATA-1TB-SPECTRIX-XPG-S40G-GEN-3X4-M2-2280",
     "image_2": "",
@@ -26,7 +26,7 @@ const almacenamiento=[
   ];
 const sillas =[
   {
-    "Id": 16,
+    "id": 16,
     "name": "Silla Gamer Thunderx3 Tc3 Jet Black (By Aerocool)",
     "image": "SILLA-GAMER-THUNDERX3-TC3-JET-BLACK-BY-AEROCOOL",
     "image_2": "",
@@ -38,7 +38,7 @@ const sillas =[
     "caracteristics": ""
   },
   {
-    "Id": 17,
+    "id": 17,
     "name": "Silla Gamer Thunderx3 Tc3 Ember Red (By Aerocool)",
     "image": "SILLA-GAMER-THUNDERX3-TC3-EMBER-RED-BY-AEROCOOL",
     "image_2": "",
@@ -52,7 +52,7 @@ const sillas =[
   ];
 const periferico =[
   {
-    "Id": 8,
+    "id": 8,
     "name": "Mouse Steelseries Rival 650 Wireless",
     "image": "steelseries-rival650-001",
     "image_2": "steelseries-rival650-002",
@@ -64,7 +64,7 @@ const periferico =[
     "caracteristics": "Sensor óptico exclusivo TrueMove3 + 12.000 CPI, 350 IPS Características Interruptores mecánicos de disparador dividido de 60 millones de clics Agarres laterales de silicona para mayor agarre y durabilidad. Iluminación RGB de 8 zonas con notificaciones Discord Personalización total con SteelSeries. Duración de la batería de más de 24 horas con carga rápida"
   },
   {
-    "Id": 12,
+    "id": 12,
     "name": "Razer Blackwidow V3 Mini Teclado Mecánico Inalambrico 65%",
     "image": "razer-blackwidow-mini-v3-001",
     "image_2": "",
@@ -76,7 +76,7 @@ const periferico =[
     "caracteristics": "Marca: Razer\\nLínea: Huntsman\\nModelo: Huntsman Mini\\nColor del teclado: Negro\\nColor de la retroiluminación: RGB\\nLayout: QWERTY\\nTipo de switch: Razer Purple\\nEs resistente a salpicaduras: Sí\\nCon cable removible: Sí\\nCon reposa muñeca: No\\nCon touchpad: No\\nCon pad numérico: No\\nCon trackball: No\\nArquitectura: Opto-Mecánico\\nEs gamer: Sí\\nCon retroiluminación: Sí\\nCon pantalla: No\\nnIncluye pilas: No\\nEs para una mano: No\\nTipo de conector: USB-C\\nCon recepción inalámbrica: Sí\\nCon interfaz USB: No\\nCon Bluetooth: No\\nForma de las teclas: Cilíndrica\\nCon teclas de acceso rápido: Sí\\nCon teclas anti-ghost: Sí"
   },
   {
-    "Id": 18,
+    "id": 18,
     "name": "Monitor Benq Led 24 Gw2480 24W Black",
     "image": "MONITOR-BENQ-LED-24-GW2480-24W-BLACK",
     "image_2": "",
@@ -88,7 +88,7 @@ const periferico =[
     "caracteristics": "Tamaño de pantalla: 23.8\\nTipo de panel: IPS\\nTecnología de retroiluminación: Retroiluminación LED\\nResolución: Full HD (1920x1080 píxeles)\\nBrillo: 250\\nContraste nativo (típico): 1000:1\\nÁngulo de visión (I/D; A/B) (CR>=10): 178°/178°\\nTiempo de respuesta: 8ms, 5ms(GtG)\\nTasa de actualización: 60Hz\\nRelación de aspecto: 16:9\\nColores de pantalla: 16.7 millones de colores\\nGama de color: 72% NTSC\\nÁrea de visualización (mm): 527.04x296.46\\nPPP: 93\\nDCR (Dynamic Contrast Ratio, relación de contraste dinámico) (típico) 20,000,000 : 1\\nBit de color: 8 bit"
   },
   {
-    "Id": 19,
+    "id": 19,
     "name": "Monitor Gamer Gigabyte 24 G24F-Sa 165Hz Ips Gaming Full Hd",
     "image": "MONITOR-GAMER-GIGABYTE-24-G24FSA-165HZ-IPS-GAMING-FULL-HD",
     "image_2": "",
@@ -102,7 +102,7 @@ const periferico =[
   ];
 const componentes =[
   {
-    "Id": 9,
+    "id": 9,
     "name": "Gabinete Kolink Inspire K20 Vidrio Templado 1X120Mm",
     "image": "kolink-inspire-k20-001",
     "image_2": "kolink-inspire-k20-002",
@@ -114,7 +114,7 @@ const componentes =[
     "caracteristics": "Panel frontal acentuado en fibra de carbono con un acabado de tiras ARGB en forma de X multiangulares\\nVentilador trasero de 120 mm preinstalado con iluminación RGB direccionable digitalmente\\nOpción de sincronización con el software de la placa base de serie\\nPanel lateral de cristal templado para una visión sin obstáculos del interior\\nDiseñada para placas base ATX/Micro-ATX-/Mini-ITX\\nAdmite radiadores de hasta 240 mm en la parte delantera y 120 mm en la trasera\\nPanel de E/S con 1x USB3.0 y 2x USB2.0, audio HD y botón RGB\\nSoporta tarjetas gráficas de hasta 310mm, refrigeradores de CPU de hasta 160mm, fuentes de alimentación de hasta 180mm"
   },
   {
-    "Id": 11,
+    "id": 11,
     "name": "Placa De Video Gigabyte Radeon Rx 5700 Xt 8G Ddr6 Gaming Oc",
     "image": "PLACA-DE-VIDEO-GIGABYTE-RADEON-RX-5700-XT-8G-DDR6-GAMING-OC",
     "image_2": "",
@@ -126,7 +126,7 @@ const componentes =[
     "caracteristics": ""
   },
   {
-    "Id": 13,
+    "id": 13,
     "name": "Gabinete Sentey P20",
     "image": "gabinete-sentey-p20-001",
     "image_2": "",
@@ -138,7 +138,7 @@ const componentes =[
     "caracteristics": "EXTERIOR\\nColor: Negro\\nFrente: Negro RGB\\nPatas: Si\\nPanel Lateral: Transparente\\nTopcover: Si\\nDisplay LCD: No\\nTornillos manuales: Si\\nEXPANSIONES\\nBahias ext 5.25”: No\\nBahias ext 3.5”: No\\nBahias int 3.5”: Si\\nBahias int 2.5”: Si\\nExpansion Slot PCI: Si\\nPUERTOS FRONTALES\\nUSB 3.0: Si\\nUSB 2.0: Si\\nAudio Frontal: Si\\nE-sata: No\\nSata Nativo: No\\nCard Reader: No\\nControlador velocidad cooler: No"
   },
   {
-    "Id": 14,
+    "id": 14,
     "name": "Cooler Mf120 Halo",
     "image": "Cooler-MF120-HALO",
     "image_2": "",
@@ -150,7 +150,7 @@ const componentes =[
     "caracteristics": ""
   },
   {
-    "Id": 15,
+    "id": 15,
     "name": "Placa De Video Amd Rx 580 8Gb Powercolor Red Devil",
     "image": "PLACA-DE-VIDEO-AMD-RX-580-8GB-POWERCOLOR-RED-DEVIL",
     "image_2": "",
@@ -162,7 +162,7 @@ const componentes =[
     "caracteristics": "Unidades de cómputo: 36\\nFrecuencia base: 1257 MHz\\nFrecuencia de aumento: Hasta 1340 MHz\\nVelocidad máx. de relleno de píxel: Hasta 42.88 GP/s\\nVelocidad máx. de relleno de textura: Hasta 192.96 GT/s\\nRendimiento máx.: Hasta 6.2 TFLOPs\\nROP: 32\\nProcesadores de transmisión: 2304\\nUnidades de textura: 144\\nRecuento de transistores: 5.7 B\\nPotencia típica de la placa (escritorio): 185 W\\nRecomendación de PSU: 500 W\\nVelocidad de memoria (efectiva): 8 Gbps\\nTamaño de memoria máx.: 8 GB\\nTipo de memoria: GDDR5\\nInterfaz de memoria: 256-bit\\nAncho de banda de memoria máx.: Hasta 256 GB/s"
   },
   {
-    "Id": 20,
+    "id": 20,
     "name": "Joystick Playstation Ps4 Dualshock Gold",
     "image": "JOYSTICK-PLAYSTATION-PS4-DUALSHOCK-GOLD",
     "image_2": "",
@@ -177,7 +177,7 @@ const componentes =[
 
 const productos = [  
   {
-  "Id": 1,
+  "id": 1,
   "name": "Asus Rog Zephyrus Duo Ryzen 9 16Gb 1Tb Rtx 3060",
   "image": "asus-zephyrus-duo_15-GX550LXS-001",
   "image_2": "asus-zephyrus-duo_15-GX550LXS-002",
@@ -189,7 +189,7 @@ const productos = [
   "caracteristics": ""
 },
 {
-  "Id": 2,
+  "id": 2,
   "name": "Notebook Gamer Hp Omen 15-Dc1020Nr",
   "image": "hp-omen-1020-001",
   "image_2": "hp-omen-1020-002",
@@ -201,7 +201,7 @@ const productos = [
   "caracteristics": "Video Card: NVIDIA® GeForce RTX™ 3070 Graphics, 8GB\\nWindows 11 Home x 64 bits, preinstalado.\\nPeso 2.45Kg. - HP Wide Vision 720p HD Webcam with Integrated Dual Array Digital Microphones"
 },
 {
-  "Id": 3,
+  "id": 3,
   "name": "Notebook Gamer Acer Nitro I5 10Ma 8Gb Ssd256 Gtx1650",
   "image": "acer-nitro-i5-001",
   "image_2": "",
@@ -213,7 +213,7 @@ const productos = [
   "caracteristics": "Procesador Intel Core i5 11-400H\\nPantalla 15.6\" FHD IPS 144Hz\\nResolución 1920 x 1080 (Full HD)\\nGráficos Placa de video dedicada Nvidia Geforce GTX 1650 4GB DDR6\\nMemoria 8GB DDR4 3200 (ampliable hasta 64GB)\\nDisco 256 GB SSD\\nAmpliaciones de disco SSD M.2 hasta 2TB / SSD o HDD 2.5\" hasta 2TB\\nConectividad inalámbrica Wi-Fi 802.11AX + Bluetooth\\nSistema operativo Windows 11 Home en inglés configurable en español\\nTeclado retroiluminado en inglés configurable español\\nPuertos 1 Ethernet, 1 HDMI, 4 USB ( 3 USB 3.2 y 1 Type C).\\nCámara frontal 720dpi\\nSonido 5.1\\nGarantía XTR Directo 6 meses"
 },
 {
-  "Id": 4,
+  "id": 4,
   "name": "Notebook Gamer Dell Alienware 17 R5 17.3",
   "image": "alienware-17-r5-001",
   "image_2": "dell-g5-5550-001",
@@ -229,7 +229,7 @@ const productos = [
 
 const notebooks = [
   {
-    "Id": 1,
+    "id": 1,
     "name": "Asus Rog Zephyrus Duo Ryzen 9 16Gb 1Tb Rtx 3060",
     "image": "asus-zephyrus-duo_15-GX550LXS-001",
     "image_2": "asus-zephyrus-duo_15-GX550LXS-002",
@@ -241,7 +241,7 @@ const notebooks = [
     "caracteristics": ""
   },
   {
-    "Id": 2,
+    "id": 2,
     "name": "Notebook Gamer Hp Omen 15-Dc1020Nr",
     "image": "hp-omen-1020-001",
     "image_2": "hp-omen-1020-002",
@@ -253,7 +253,7 @@ const notebooks = [
     "caracteristics": "Video Card: NVIDIA® GeForce RTX™ 3070 Graphics, 8GB\\nWindows 11 Home x 64 bits, preinstalado.\\nPeso 2.45Kg. - HP Wide Vision 720p HD Webcam with Integrated Dual Array Digital Microphones"
   },
   {
-    "Id": 3,
+    "id": 3,
     "name": "Notebook Gamer Acer Nitro I5 10Ma 8Gb Ssd256 Gtx1650",
     "image": "acer-nitro-i5-001",
     "image_2": "",
@@ -265,7 +265,7 @@ const notebooks = [
     "caracteristics": "Procesador Intel Core i5 11-400H\\nPantalla 15.6\" FHD IPS 144Hz\\nResolución 1920 x 1080 (Full HD)\\nGráficos Placa de video dedicada Nvidia Geforce GTX 1650 4GB DDR6\\nMemoria 8GB DDR4 3200 (ampliable hasta 64GB)\\nDisco 256 GB SSD\\nAmpliaciones de disco SSD M.2 hasta 2TB / SSD o HDD 2.5\" hasta 2TB\\nConectividad inalámbrica Wi-Fi 802.11AX + Bluetooth\\nSistema operativo Windows 11 Home en inglés configurable en español\\nTeclado retroiluminado en inglés configurable español\\nPuertos 1 Ethernet, 1 HDMI, 4 USB ( 3 USB 3.2 y 1 Type C).\\nCámara frontal 720dpi\\nSonido 5.1\\nGarantía XTR Directo 6 meses"
   },
   {
-    "Id": 4,
+    "id": 4,
     "name": "Notebook Gamer Dell Alienware 17 R5 17.3",
     "image": "alienware-17-r5-001",
     "image_2": "dell-g5-5550-001",
@@ -277,7 +277,7 @@ const notebooks = [
     "caracteristics": "8ª generación Intel Core i7-8750H (6 núcleos, 9 MB de caché, hasta 4. 1 GHz con Turbo Boost)\\n17. 3 pulgadas FHD (1920 x 1080) 60 Hz IPS antirreflejos 300-nits | NVIDIA GeForce GTX 1070 con 8 GB GDDR5\\n16 GB, DDR4, 2400 MHz\\nUnidad de estado sólido de 256 GB + disco duro de 1 TB 7200 rpm\\nBatería de iones de litio (99 Why) | Windows 10 Home"
   },
   {
-    "Id": 5,
+    "id": 5,
     "name": "Notebook Nsx Kairos I5 16G Ssd250 W10P+O",
     "image": "NOTEBOOK-NSX-KAIROS-I5-16G-SSD250-W10PO",
     "image_2": "",
@@ -289,7 +289,7 @@ const notebooks = [
     "caracteristics": "Marca: NSX\\nLínea: Kairos\\nResolución de la pantalla: 1920 px x 1080 px\\nCon pantalla táctil: No\\nTipo de resolución: Full HD\\nTamaño de la pantalla: 14 \"\\nCon Wi-Fi: Sí\\nPeso: 1.5 kg\\nMemoria RAM: 16 GB\\nTipo de memoria RAM: DDR4\\nCapacidad del SSD: 250 GB\\nMarca del procesador: Intel\\nLínea del procesador: Core i5\\nCantidad de núcleos: 2\\nNombre del sistema operativo: FreeDOS\\nTipo de batería: Ion de litio\\nEs gamer: No"
   },
   {
-    "Id": 6,
+    "id": 6,
     "name": "Notebook Gigabyte AORUS RTX 3080Q",
     "image": "Notebook-Gigabyte-AORUS-RTX-3080Q",
     "image_2": "",
@@ -301,7 +301,7 @@ const notebooks = [
     "caracteristics": "Procesador: Intel Core i7-11800H (11.ª generación)\\nUPC: 2,3 a 4,6 GHz 8 núcleos\\nCaché L3: 24 MB\\nTipo de gráficos: Dedicado\\nGPU: NVIDIA GeForce RTX 3080 con 8 GB GDDR6 VRAM\\nTipo de memoria: DDR4 de 3200 MHz\\nMemoria total instalada: 32GB\\nConfiguración de memoria: 2 x 16 GB\\nCapacidad máxima de memoria: 64GB\\nTipo de ranura de memoria: DIMM SO de 260 pines"
   },
   {
-    "Id": 7,
+    "id": 7,
     "name": "Notebook MSI GL66 Pulse 11UEK-801AR",
     "image": "MSI-NOTEBOOK-GAMER-GL66-PULSE-11UEK801AR",
     "image_2": "MSI-NOTEBOOK-GAMER-GL66-PULSE-11UEK801AR-1",
@@ -328,7 +328,12 @@ const mainController = {
 
     productDetail:function(req,res){
         res.locals.title = "DFT - Detalle del Producto"
-        res.render('products/productDetail', {productos : productos});
+
+        const producto = productos.find(producto =>{
+          return producto.id === parseInt(req.params.id);          
+        });
+        console.log(producto);
+        res.render('products/productDetail', {producto : producto, productos : productos});
     },
 
     productList:function(req,res){
