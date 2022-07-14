c4cffd6b2bc44107accf539753413f7fe182d40a
pali <paulayafar.86@gmail.com>
2022-07-13 23:17:32 -0300

    test remote db

--- database/config/config.js
+++ database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "mhs8y04cxpkjk1v0",
+    "password": "ccn6hi1btn48ndvg",
+    "database": "mwizm4g504p3z8j4",
+    "host": "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "mhs8y04cxpkjk1v0",
+    "password": "ccn6hi1btn48ndvg",
+    "database": "mwizm4g504p3z8j4",
+    "host": "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
