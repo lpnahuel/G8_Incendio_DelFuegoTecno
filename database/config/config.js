e395f1b964af2db90ba866b0eb3fe88efec8b7fd
pali <paulayafar.86@gmail.com>
2022-07-13 23:01:06 -0300

    base de datos online

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "sqlpassword123",
-    "database": "dftechno-db",
-    "host": "127.0.0.1",
+    "username": "mhs8y04cxpkjk1v0",
+    "password": "ccn6hi1btn48ndvg",
+    "database": "mwizm4g504p3z8j4",
+    "host": "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "test": {
